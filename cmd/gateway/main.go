742bb5669f4a4604734f5c69efe348bb49a71b91
Takahiro55555 <tkhr.corei7@gmail.com>
2020-11-24 07:54:08 +0900

    refactor: ディレクトリ構成を修正 参考: https://qiita.com/sueken/items/87093e5941bfbc09bea8

---
+++ cmd/gateway/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"gateway/internal/apps/gateway"
+)
+
+func main() {
+	gateway.Gateway()
+}
---
+++ internal/apps/gateway/gateway_internal_test.go
@@ -0,0 +1,9 @@
+package gateway
+
+import (
+	"testing"
+)
+
+func TestGateWay(t *testing.T) {
+	t.Skip("skip main...")
+}
