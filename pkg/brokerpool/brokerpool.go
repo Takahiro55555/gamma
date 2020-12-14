6c6c884d549ad89fccc4853c9ddc5380e7fba40a
Takahiro55555 <tkhr.corei7@gmail.com>
2020-12-14 16:40:56 +0900

    modify: broker 構造体関連のコードの移動に伴い該当箇所を削除

--- pkg/brokerpool/brokerpool.go
+++ pkg/brokerpool/brokerpool.go
@@ -2,20 +2,19 @@ package brokerpool
 
 import (
 	"fmt"
+	"gateway/pkg/broker"
 	"reflect"
 	"sync"
 	"time"
-
-	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
 /*************  　  以下 brokerpool 構造体関連  　  *************/
 
 // Brokerpool is the interface definition
 type Brokerpool interface {
-	GetBroker(host string, port uint16) (Broker, error)
+	GetBroker(host string, port uint16) (broker.Broker, error)
 	ConnectBroker(host string, port uint16) error
-	GetOrConnectBroker(host string, port uint16) (Broker, error)
+	GetOrConnectBroker(host string, port uint16) (broker.Broker, error)
 	TryDisconnectBroker(host string, port uint16, expirationFromLastPub time.Duration, quiesce uint) bool
 	IncreaseSubCnt(host string, port uint16) error
 	DecreaseSubCnt(host string, port uint16) error
@@ -33,7 +32,7 @@ func NewBrokerPool() Brokerpool {
 	return &brokerpool{}
 }
 
-func (p *brokerpool) GetBroker(host string, port uint16) (Broker, error) {
+func (p *brokerpool) GetBroker(host string, port uint16) (broker.Broker, error) {
 	bt, err := p.bt.Load(host)
 	if err != nil {
 		return nil, err
@@ -58,12 +57,12 @@ func (p *brokerpool) ConnectBroker(host string, port uint16) error {
 	}
 
 	// ブローカへの接続を試みる
-	b, err = connectBroker(host, port)
+	b, err = broker.ConnectBroker(host, port)
 	if err != nil {
 		return err
 	}
 
-	// brokerpool へ Broker インターフェースを登録する
+	// brokerpool へ broker.Broker インターフェースを登録する
 	bt, err := p.bt.Load(host)
 	if err != nil && reflect.ValueOf(err).Type() == typeNotFoundErr {
 		bt = &BrokerTableByPort{}
@@ -75,7 +74,7 @@ func (p *brokerpool) ConnectBroker(host string, port uint16) error {
 	return nil
 }
 
-func (p *brokerpool) GetOrConnectBroker(host string, port uint16) (Broker, error) {
+func (p *brokerpool) GetOrConnectBroker(host string, port uint16) (broker.Broker, error) {
 	b, err := p.GetBroker(host, port)
 	if err == nil {
 		return b, err
@@ -143,98 +142,6 @@ func (p *brokerpool) UpdateLastPub(host string, port uint16) error {
 }
 
 /*************  　  以上 brokerpool 構造体関連  　  *************/
-/*************  　    以下 broker 構造体関連  　    *************/
-
-// HACK: broker 構造体関連は別パッケージにしたほうが良さげ
-
-// Broker is the interface definition
-type Broker interface {
-	TryDisconnect(expirationFromLastPub time.Duration, quiesce uint) bool
-	IncreaseSubCnt() error
-	DecreaseSubCnt() error
-	GetSubCnt() uint
-	UpdateLastPub()
-	GetLastPub() time.Time
-}
-
-// 分散ブローカに関するデータを管理する構造体
-type broker struct {
-	Client    mqtt.Client
-	SubCntMu  sync.RWMutex
-	SubCnt    uint // 接続先分散ブローカーへ Subscribe 要求している MQTT クライアントの数
-	LastPubMu sync.RWMutex
-	LastPub   time.Time // 接続先分散ブローカーへ MQTT クライアントが最後に Publish 要求をした時刻
-}
-
-func newBroker(c mqtt.Client) Broker {
-	return &broker{
-		Client:  c,
-		SubCnt:  0,
-		LastPub: time.Now(),
-	}
-}
-
-func connectBroker(host string, port uint16) (Broker, error) {
-	opts := mqtt.NewClientOptions()
-	opts.AddBroker(fmt.Sprintf("tcp://%v:%v", host, port))
-	c := mqtt.NewClient(opts)
-	b := newBroker(c)
-	return b, nil
-}
-
-func (b *broker) TryDisconnect(expirationFromLastPub time.Duration, quiesce uint) bool {
-	if b.GetSubCnt() != 0 {
-		return false
-	}
-	if b.GetLastPub().Add(expirationFromLastPub).Before(time.Now()) {
-		return false
-	}
-	b.Client.Disconnect(quiesce)
-	return true
-}
-
-func (b *broker) IncreaseSubCnt() error {
-	var maxSubCnt uint = 0xffffffff
-	b.SubCntMu.Lock()
-	defer b.SubCntMu.Unlock()
-
-	if b.SubCnt < maxSubCnt {
-		b.SubCnt++
-		return nil
-	}
-	return MaxSubCntError{Msg: fmt.Sprintf("Already reached max SubCnt (%v)", maxSubCnt)}
-}
-
-func (b *broker) DecreaseSubCnt() error {
-	b.SubCntMu.Lock()
-	defer b.SubCntMu.Unlock()
-
-	if b.SubCnt > 0 {
-		b.SubCnt--
-		return nil
-	}
-	return ZeroSubCntError{Msg: "SubCnt is already zero"}
-}
-
-func (b *broker) GetSubCnt() uint {
-	b.SubCntMu.RLock()
-	defer b.SubCntMu.RUnlock()
-	return b.SubCnt
-}
-
-func (b *broker) UpdateLastPub() {
-	b.LastPubMu.Lock()
-	defer b.LastPubMu.Unlock()
-	b.LastPub = time.Now()
-}
-
-func (b *broker) GetLastPub() time.Time {
-	b.LastPubMu.RLock()
-	defer b.LastPubMu.RUnlock()
-	return b.LastPub
-}
-
-/*************  　    以上 broker 構造体関連       *************/
 /*************　以下 BrokersTableByHost 構造体関連 *************/
 
 // BrokersTableByHost 構造体を管理する構造体 (map)
@@ -279,20 +186,20 @@ func newBrokerTableByPort() *BrokerTableByPort {
 }
 
 // Store 関数
-func (s *BrokerTableByPort) Store(key uint16, value Broker) {
+func (s *BrokerTableByPort) Store(key uint16, value broker.Broker) {
 	s.t.Store(key, value)
 }
 
 // Load 関数
-func (s *BrokerTableByPort) Load(key uint16) (Broker, error) {
+func (s *BrokerTableByPort) Load(key uint16) (broker.Broker, error) {
 	v, ok := s.t.Load(key)
 	if !ok {
 		return nil, NotFoundError{Msg: fmt.Sprintf("Not found (key = %v)", key)}
 	}
 
-	t, ok := v.(Broker)
+	t, ok := v.(broker.Broker)
 	if !ok {
-		return nil, StoredTypeIsInvalidError{Msg: fmt.Sprintf("Stored type is invalid (expected = %T, result = %T)", broker{}, v)}
+		return nil, StoredTypeIsInvalidError{Msg: fmt.Sprintf("Stored type is invalid (expected = %T, result = %T)", broker.NewBroker(nil), v)}
 	}
 
 	return t, nil
@@ -328,26 +235,6 @@ type StoredTypeIsInvalidError struct {
 }
 
 func (e StoredTypeIsInvalidError) Error() string {
-	return fmt.Sprintf("Error: %v", e.Msg)
-}
-
-// MaxSubCntError 構造体
-// 当該ブローカに設定された subscriber 上限に達した際に返される
-type MaxSubCntError struct {
-	Msg string
-}
-
-func (e MaxSubCntError) Error() string {
-	return fmt.Sprintf("Error: %v", e.Msg)
-}
-
-// ZeroSubCntError 構造体
-// 当該ブローカの subscriber が存在しないにもかかわらず、DecreaseSubCnt 関数を呼び出してしまった際に返される
-type ZeroSubCntError struct {
-	Msg string
-}
-
-func (e ZeroSubCntError) Error() string {
 	return fmt.Sprintf("Error: %v", e.Msg)
 }
 
