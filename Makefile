c83832cf33719d1d77b0a257c368566e5c19dcf5
Takahiro55555 <tkhr.corei7@gmail.com>
2021-01-28 03:51:23 +0900

    update: ゲートウェイとの連携を確認

---
+++ Makefile
@@ -0,0 +1,46 @@
+# Go パラメータ
+GOCMD=go
+GOBUILD=$(GOCMD) build
+GOCLEAN=$(GOCMD) clean
+GOTEST=$(GOCMD) test
+GORUN=$(GOCMD) run
+GO_ENTRY_POINT_GATEWAY=cmd/gateway/main.go
+BINARY_NAME=gateway.out
+COVERAGE_FILE=cover.out
+COVERAGE_FILE_HTML=cover.html
+DOCKER_COMPOSE=docker-compose
+TEST_DOCKER_COMPOSE_FILE=./build/docker-compose.yml
+TEST_DOCKER_COMPOSE_UP_D=$(DOCKER_COMPOSE) -f $(TEST_DOCKER_COMPOSE_FILE) up -d
+TEST_DOCKER_COMPOSE_DOWN=$(DOCKER_COMPOSE) -f $(TEST_DOCKER_COMPOSE_FILE) down
+level=warn
+env=dev
+caller= 
+host=localhost
+port=1883
+
+# Windows, WSL を想定
+HTML_OPEN_CMD=explorer.exe
+ifeq ($(shell uname),Darwin)
+	# MacOS を想定
+	HTML_OPEN_CMD=open -a "Safari"
+endif
+
+all: test build
+.PHONY: build  # 擬似ターゲット
+build:
+	$(GOBUILD) -o $(BINARY_NAME) $(GO_ENTRY_POINT_GATEWAY)
+.PHONY: test  # 擬似ターゲット
+test:
+	$(GOTEST) -coverprofile=$(COVERAGE_FILE) ./...
+coverage:
+	$(GOTEST) -coverprofile=$(COVERAGE_FILE) ./...
+	go tool cover -html=$(COVERAGE_FILE) -o $(COVERAGE_FILE_HTML)
+	$(HTML_OPEN_CMD) $(COVERAGE_FILE_HTML)
+clean:
+	$(GOCLEAN) ./...
+	rm -f $(BINARY_NAME)
+	rm -f $(COVERAGE_FILE) $(COVERAGE_FILE_HTML)
+	$(TEST_DOCKER_COMPOSE_DOWN)
+run: clean
+	$(TEST_DOCKER_COMPOSE_UP_D)
+	$(GORUN) cmd/manager/main.go -level ${level} -env ${env} ${caller} -host $(host) -port $(port)
--- internal/apps/manager/manager.go
+++ internal/apps/manager/manager.go
@@ -1,7 +1,232 @@
 package manager
 
-import mqtt "github.com/eclipse/paho.mqtt.golang"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"os/signal"
+	"sort"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	log "github.com/sirupsen/logrus"
+)
+
+type BrokerInfo struct {
+	Host string `json:"host"`
+	Port uint16 `json:"port"`
+}
+
+type DistributedBrokerInfo struct {
+	Topic      string     `json:"topic"`
+	BrokerInfo BrokerInfo `json:"broker_info"`
+}
+
+type AllDistributedBrokerInfo struct {
+	Version int                     `json:"version"`
+	DMBs    []DistributedBrokerInfo `json:"brokers"`
+}
+
+type GatewayBrokerInfo struct {
+	Topic      string     `json:"topic"`
+	BrokerInfo BrokerInfo `json:"broker_info"`
+}
+
+type GatewayBrokerStatus struct {
+	Status     string     `json:"status"`
+	Version    int        `json:"version"`
+	BrokerInfo BrokerInfo `json:"broker_info"`
+}
 
 func Manager(client mqtt.Client) {
-	_ = client
+	startTimeUnix := time.Now().Unix()
+
+	// プルグラムを強制通知を受け取るためのチャンネル
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, os.Interrupt)
+
+	// 1秒おきに統計情報などをログに出力するためのタイマ
+	metricsTicker := time.NewTicker(time.Second)
+
+	// Gatewayの分散ブローカ情報追加結果を受け取るチャンネル
+	// brokertableUpdateResultMsgCh := make(chan mqtt.Message, 10)
+	// var brokertableUpdateResultMsgFunc mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+	// 	brokertableUpdateResultMsgCh <- msg
+	// }
+	// if token := client.Subscribe("/api/brokertable/update/result", 2, brokertableUpdateResultMsgFunc); token.Wait() && token.Error() != nil {
+	// 	log.WithFields(log.Fields{"error": token.Error()}).Fatal("MQTT subscribe error")
+	// }
+
+	// Gatewayの状態通知を受取るチャンネル
+	gatewayNotifyMsgCh := make(chan mqtt.Message, 10)
+	var gatewayNotifyMsgFunc mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+		gatewayNotifyMsgCh <- msg
+	}
+	if token := client.Subscribe("/api/notice/gatewaybroker", 2, gatewayNotifyMsgFunc); token.Wait() && token.Error() != nil {
+		log.WithFields(log.Fields{"error": token.Error()}).Fatal("MQTT subscribe error")
+	}
+
+	// ゲートウェイブローカの担当エリア設定リクエストを受取るチャンネル
+	setGatewayBrokerMsgCh := make(chan mqtt.Message, 10)
+	var setGatewayBrokerMsgFunc mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+		setGatewayBrokerMsgCh <- msg
+	}
+	if token := client.Subscribe("/api/tool/gatewaybroker/set", 1, setGatewayBrokerMsgFunc); token.Wait() && token.Error() != nil {
+		log.WithFields(log.Fields{"error": token.Error()}).Fatal("MQTT subscribe error")
+	}
+
+	// 分散ブローカの追加リクエストを受取るチャンネル
+	addDistributedBrokerMsgCh := make(chan mqtt.Message, 10)
+	var addDistributedBrokerMsgFunc mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+		addDistributedBrokerMsgCh <- msg
+	}
+	if token := client.Subscribe("/api/tool/distributedbroker/add", 1, addDistributedBrokerMsgFunc); token.Wait() && token.Error() != nil {
+		log.WithFields(log.Fields{"error": token.Error()}).Fatal("MQTT subscribe error")
+	}
+
+	// 分散ブローカ情報更新の際に使用する
+	gatewayCoverAreaInfo := map[string]GatewayBrokerInfo{}
+	gatewayStatusMap := map[string]GatewayBrokerStatus{}
+	allDistributedBrokerList := AllDistributedBrokerInfo{Version: -1, DMBs: []DistributedBrokerInfo{}}
+	isUpdatingDistributedBrokerList := false
+
+	for {
+		select {
+		// Gatewayの状態通知を受取るチャンネル
+		case m := <-gatewayNotifyMsgCh:
+			log.WithFields(log.Fields{"payload": string(m.Payload())}).Trace("gatewayNotifyMsgCh")
+			// JSONデコード
+			var gatewayStatus GatewayBrokerStatus
+			if err := json.Unmarshal(m.Payload(), &gatewayStatus); err != nil {
+				log.WithFields(log.Fields{"err": err}).Fatal("add gateway broker (gatewayNotifyMsgCh)")
+			}
+			key := fmt.Sprintf("%v-%v", gatewayStatus.BrokerInfo.Host, gatewayStatus.BrokerInfo.Port)
+			if _, ok := gatewayCoverAreaInfo[key]; !ok {
+				gatewayCoverAreaInfo[key] = GatewayBrokerInfo{Topic: "/", BrokerInfo: gatewayStatus.BrokerInfo}
+			}
+			gatewayStatusMap[key] = gatewayStatus
+			if gatewayStatus.Status == "up" {
+				var payload []GatewayBrokerInfo
+				for _, v := range gatewayCoverAreaInfo {
+					payload = append(payload, v)
+				}
+				msg, err := json.Marshal(payload)
+				if err != nil {
+					log.WithFields(log.Fields{"err": err}).Fatal("add gateway broker (gatewayNotifyMsgCh)")
+				}
+				if token := client.Publish("/api/gateway/info/all", 2, true, msg); token.Wait() && token.Error() != nil {
+					log.WithFields(log.Fields{"error": token.Error()}).Fatal("Notify to manager")
+				}
+			}
+
+			// 全ての Gateway の状態が completeかどうかを確かめる
+			for _, info := range gatewayStatusMap {
+				isUpdatingDistributedBrokerList = false
+				if info.Version != allDistributedBrokerList.Version {
+					isUpdatingDistributedBrokerList = true
+					break
+				}
+			}
+			if !isUpdatingDistributedBrokerList {
+				if token := client.Publish("/api/brokertable/update/status", 2, false, "complete"); token.Wait() && token.Error() != nil {
+					log.WithFields(log.Fields{"error": token.Error()}).Fatal("MQTT publish error")
+				}
+				log.Info("Distributed broker`s info update complete ")
+			}
+
+		// ユーザによるゲートウェイ担当エリアの設定
+		case m := <-setGatewayBrokerMsgCh:
+			log.WithFields(log.Fields{"payload": string(m.Payload())}).Trace("setGatewayBrokerMsgCh")
+			// JSONデコード
+			var gatewayCoverArea GatewayBrokerInfo
+			if err := json.Unmarshal(m.Payload(), &gatewayCoverArea); err != nil {
+				log.WithFields(log.Fields{"err": err}).Fatal("add gateway broker (gatewayNotifyMsgCh)")
+			}
+			key := fmt.Sprintf("%v-%v", gatewayCoverArea.BrokerInfo.Host, gatewayCoverArea.BrokerInfo.Port)
+			if _, ok := gatewayCoverAreaInfo[key]; !ok {
+				log.WithFields(log.Fields{
+					"Host": gatewayCoverArea.BrokerInfo.Host,
+					"Port": gatewayCoverArea.BrokerInfo.Port,
+				}).Error("This gateway is not exists...")
+				continue
+			}
+			gatewayCoverAreaInfo[key] = gatewayCoverArea
+
+			var payload []GatewayBrokerInfo
+			for _, v := range gatewayCoverAreaInfo {
+				payload = append(payload, v)
+			}
+			msg, err := json.Marshal(payload)
+			if err != nil {
+				log.WithFields(log.Fields{"err": err}).Fatal("add gateway broker (gatewayNotifyMsgCh)")
+			}
+			if token := client.Publish("/api/gateway/info/all", 2, true, msg); token.Wait() && token.Error() != nil {
+				log.WithFields(log.Fields{"error": token.Error()}).Fatal("Notify to manager")
+			}
+			log.WithFields(log.Fields{
+				"Host": gatewayCoverArea.BrokerInfo.Host,
+				"Port": gatewayCoverArea.BrokerInfo.Port,
+			}).Info("Updated cover area info")
+
+		// ユーザによる分散ブローカの登録（「出来なかったらやり直せばいいでしょ」の方針）
+		case m := <-addDistributedBrokerMsgCh:
+			log.WithFields(log.Fields{"payload": string(m.Payload())}).Trace("addDistributedBrokerMsgCh")
+			if isUpdatingDistributedBrokerList {
+				log.WithFields(log.Fields{
+					"isUpdatingDistributedBrokerList": isUpdatingDistributedBrokerList,
+					"gatewayStatusMap":                gatewayStatusMap,
+				}).Error("Could not add distributed broker...")
+				continue
+			}
+
+			isUpdatingDistributedBrokerList = true
+			allDistributedBrokerList.Version++
+			// JSONデコード
+			var newDistributedBrokerInfo DistributedBrokerInfo
+			if err := json.Unmarshal(m.Payload(), &newDistributedBrokerInfo); err != nil {
+				log.WithFields(log.Fields{"err": err}).Fatal("add distributed broker (addDistributedBrokerMsgCh)")
+			}
+			// バリデーションを行う
+			for _, info := range allDistributedBrokerList.DMBs {
+				if info.BrokerInfo.Host == newDistributedBrokerInfo.BrokerInfo.Host && info.BrokerInfo.Port == newDistributedBrokerInfo.BrokerInfo.Port {
+					isUpdatingDistributedBrokerList = false
+					log.WithFields(log.Fields{
+						"Host": newDistributedBrokerInfo.BrokerInfo.Host,
+						"Port": newDistributedBrokerInfo.BrokerInfo.Port,
+					}).Error("This broker is already exists (addDistributedBrokerMsgCh)")
+					break
+				}
+			}
+			allDistributedBrokerList.DMBs = append(allDistributedBrokerList.DMBs, newDistributedBrokerInfo)
+			// 非安定ソート
+			sort.Slice(allDistributedBrokerList.DMBs, func(i, j int) bool {
+				return len(allDistributedBrokerList.DMBs[i].Topic) < len(allDistributedBrokerList.DMBs[j].Topic)
+			})
+
+			// JSONエンコード
+			msg, err := json.Marshal(allDistributedBrokerList)
+			if err != nil {
+				log.WithFields(log.Fields{"err": err}).Fatal("add gateway broker (gatewayNotifyMsgCh)")
+			}
+			if token := client.Publish("/api/brokertable/all/info", 2, true, msg); token.Wait() && token.Error() != nil {
+				log.WithFields(log.Fields{"error": token.Error()}).Fatal("Notify to manager")
+			}
+
+		case <-metricsTicker.C:
+			passedTimeSecTotal := time.Now().Unix() - startTimeUnix
+			passedTimeHour := passedTimeSecTotal / 3600
+			passedTimeMinutes := (passedTimeSecTotal / 60) % 60
+			passedTimeSeconds := passedTimeSecTotal % 60
+			log.WithFields(log.Fields{
+				"hour":          passedTimeHour,
+				"minutes":       passedTimeMinutes,
+				"seconds":       passedTimeSeconds,
+				"seconds_total": passedTimeSecTotal,
+			}).Info("Total run time")
+
+		case <-signalCh:
+			log.Info("Interrupt detected.\n")
+			return
+		}
+	}
 }
