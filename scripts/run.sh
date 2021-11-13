2085419e0190b432b90e25f52dd4abfb71f985b0
Takahiro55555 <tkhr.corei7@gmail.com>
2021-11-14 04:32:10 +0900

    update: 分散ブローカの登録、gateway の担当エリアの登録処理を追加し、ログをファイルに記録するようにした

--- scripts/run.sh
+++ scripts/run.sh
@@ -15,7 +15,7 @@ gatewayPort=1884
 
 PID_FILE=".pid"
 level=info
-env=development
+env=production
 caller= 
 
 if [ -f ${PID_FILE} ]; then
@@ -23,20 +23,39 @@ if [ -f ${PID_FILE} ]; then
     ${TEST_DOCKER_COMPOSE_DOWN}
     rm ${PID_FILE}
 elif [ "${1}" = "run" ]; then
+    # MQTT ブローカの起動
     ${TEST_DOCKER_COMPOSE_DOWN}
     ${TEST_DOCKER_COMPOSE_UP_D}
+
+    # manager の起動
     go run cmd/manager/main.go -level ${level} \
                                -env ${env} ${caller} \
                                -host ${managerHost} \
-                               -port ${managerPort} &
+                               -port ${managerPort} > manager00.log &
     echo $! > ${PID_FILE}
+
+    # gateway の起動
     go run cmd/gateway/main.go -level ${level} \
                                -env ${env} ${caller} \
                                -managerHost ${managerHost} \
                                -managerPort ${managerPort} \
                                -gatewayHost ${gatewayHost} \
-                               -gatewayPort ${gatewayPort} &
+                               -gatewayPort ${gatewayPort} > gateway00.log &
     echo $! >> ${PID_FILE}
+
+    # manager と gateway を停止させるために、プロセスIDを保持する
+    # NOTE: sleep を入れないと、プロセスIDをファイルに記録することが出来なかった
     sleep 1
     ps | grep main | grep -oE "^[0-9]+" >> ${PID_FILE}
+
+    # manager へ分散ブローカを設定する
+    mosquitto_pub -h localhost -p 1883 -t "/api/tool/distributedbroker/add" -m '{"topic":"/","broker_info":{"host":"localhost","port":1893}}'
+    sleep 1
+    mosquitto_pub -h localhost -p 1883 -t "/api/tool/distributedbroker/add" -m '{"topic":"/1","broker_info":{"host":"localhost","port":1894}}'
+
+    # manager へ gateway の担当エリアを設定する
+    sleep 1
+    mosquitto_pub -h localhost -p 1883 -t "/api/tool/gatewaybroker/set" -m '{"topic":"/","broker_info":{"host":"localhost","port":1884}}'
+    sleep 1
+    mosquitto_pub -h localhost -p 1883 -t "/api/tool/gatewaybroker/set" -m '{"topic":"/0","broker_info":{"host":"localhost","port":1884}}'
 fi
